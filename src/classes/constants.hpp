936c95c1fbb07303bd975d4d3070e8b01e173bad
jramhani <jaber.ramhani@ulb.be>
2024-07-24 09:27:00 +0200

    [CLEAN] constants

--- src/classes/constants.hpp
+++ src/classes/constants.hpp
@@ -3,8 +3,14 @@
 
 
 // set console color constants
+#define BOLD "\033[1m"
+#define ITALIC "\033[3m"
+#define RESET "\033[0m"
+// colors
 #define RED "\033[31m"
 #define GREEN "\033[32m"
-#define RESET "\033[0m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define GRAY "\033[90m"
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,7 @@
 #include "classes/PEBinary.hpp"
 #include "classes/PEBinaryAlterations.cpp"
 
-#define ITALIC "\033[3m"
-#define RESET "\033[0m"
-// colors
-#define RED "\033[31m"
-#define GREEN "\033[32m"
-#define YELLOW "\033[33m"
-#define BLUE "\033[34m"
-#define GRAY "\033[90m"
+#include "classes/constants.hpp"
 
 void header(){
     // Logo art
